2b1fab9941992ea076b56a2c042be6fb66604121
cheney <cheney@sonder.io>
2023-04-10 16:33:53 +1000

    strinfy event

--- api/events_handlers/_app_general.js
+++ api/events_handlers/_app_general.js
@@ -2,7 +2,7 @@ import { postToChannel } from "../_utils"
 
 
 export async function general_req(req, res) {
-    let event = req.body.event
+    let event = JSON.stringify(req.body.event);
 
     try {
         await postToChannel("bot-debug", res, `${event}`)
