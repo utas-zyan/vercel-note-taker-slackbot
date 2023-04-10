c382a9a92cf5aed17c1c53117d01ed414a347825
cheney <cheney@sonder.io>
2023-04-10 16:55:48 +1000

    fix imports

--- api/events.js
+++ api/events.js
@@ -1,6 +1,7 @@
 import { challenge } from './events_handlers/_challenge'
 import { general_req } from './events_handlers/_app_general'
 import { app_mention } from './events_handlers/_app_mention'
+import { app_message_received } from './events_handlers/_app_messages'
 import { channel_created } from './events_handlers/_channel_created'
 import { validateSlackRequest } from './_validate'
 import { signingSecret } from './_constants'
@@ -21,7 +22,7 @@ module.exports = async (req, res) => {
             switch (event_type) {
                 case "app_mention": await app_mention(req, res); break;
                 case "channel_created": await channel_created(req, res); break;
-                case "message": await message_received(req, res); break;
+                case "message": await app_message_received(req, res); break;
                 default: console.log("unhandled event" + event_type);
             }
 
